6b5df6f733e17f2f88f4bc57d223a02ea526edd1
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-17 19:27:31 +0100

    Add fade in functioality to websites page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
-AOS.init({
-    duration: 1200,
-  })
+import AOS from '../../node_modules/aos';
+import '../../node_modules/aos/dist/aos.css';
+
+AOS.init();
