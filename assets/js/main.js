b16b571ef5913d9e49449aa413028a74aba9e074
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-19 17:30:17 +0100

    Add dropdown menu funcitonality with jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,33 @@
 
 var dropdown = document.getElementById("dropdown");
+var web_btn = document.getElementById("web_btn")
 
 // function dropdown_btn() {
 //     dropdown.classList.toggle("show");
 // }
 
 $(document).ready(function() {
+
     $("#web_btn").click(function(){
       $("#dropdown").fadeIn(1000);
+      $("#web_btn").addClass("disabled");
+      testFunction();
     });
-  });
+
+});
+
+
+function testFunction() {
+    if (web_btn.classList.contains("disabled")){
+        // closeMenu(); 
+    }
+}
+
+// $(function() {
+    $(document).on('click', function(link) {
+        if (link.target.id != 'web_btn') {
+                $("#dropdown").fadeOut(1000);
+                $("#web_btn").removeClass("disabled");
+            }
+        })
+// });
